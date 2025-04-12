65fb13cca7e3cd16c46fa38bfd95727e6ea897ef
Artie Fan <fanartie@gmail.com>
2025-04-12 20:01:46 +0000

    add routes for sourceData, sourceRecord

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
 
 import formRoutes from './routes/form'
+import sourceRecordRoutes from './routes/sourceRecord'
+import sourceDataRoutes from './routes/sourceData'
 import errorHandler from './errors'
 
 function build(opts = {}) {
   const app = fastify(opts)
 
   app.register(formRoutes, { prefix: '/form' })
+  app.register(sourceRecordRoutes, { prefix: '/sourceRecord' })
+  app.register(sourceDataRoutes, { prefix: '/sourceData' })
 
   app.setErrorHandler(errorHandler)
 
---
+++ backend/src/routes/sourceData.ts
@@ -0,0 +1,53 @@
+import { FastifyInstance } from 'fastify'
+
+import { SourceData as Entity } from '@prisma/client'
+
+import prisma from '../db/db_client'
+import { serializer } from './middleware/pre_serializer'
+import { IEntityId } from './schemas/common'
+import { ApiError } from '../errors'
+
+async function sourceDataRoutes(app: FastifyInstance) {
+  const tableName = 'sourceData'
+
+  app.setReplySerializer(serializer)
+
+  const log = app.log.child({ component: tableName + 'Routes' })
+
+  app.get<{
+    Params: IEntityId
+    Reply: Entity
+  }>('/:id', {
+    async handler(req, reply) {
+      const { params } = req
+      const { id } = params
+      log.debug('get ' + tableName + ' by id')
+      try {
+        const record = await prisma[tableName].findUniqueOrThrow({
+          where: { id },
+        })
+        reply.send(record)
+      } catch (err: any) {
+        log.error({ err }, err.message)
+        throw new ApiError('failed to fetch ' + tableName, 400)
+      }
+    },
+  })
+
+  app.get<{
+    Reply: Entity[]
+  }>('/', {
+    async handler(req, reply) {
+      log.debug('get all ', tableName)
+      try {
+        const records = await prisma[tableName].findMany()
+        reply.send(records)
+      } catch (err: any) {
+        log.error({ err }, err.message)
+        throw new ApiError('failed to fetch ' + tableName, 400)
+      }
+    },
+  })
+}
+
+export default sourceDataRoutes
